75be2526be8c7bd6b52711e94c2af0cc459cf680
farrasperdana <51940291+farrasperdana@users.noreply.github.com>
2019-07-05 16:21:36 +0700

    Update index.php

--- index.php
+++ index.php
@@ -26,7 +26,7 @@
        <input type="submit" name="load_data" value="Load Data" />
  </form>
  <?php
-    $host = "<farras@farrasdb>";
+    $host = "<farrasdb.database.windows.net>";
     $user = "<farras>";
     $pass = "<Perdana10!>";
     $db = "<farrasdb>";
